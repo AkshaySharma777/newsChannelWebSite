14b25ca573b1fb9face544c2c53975632302cd79
AkshaySharma777 <jsakshay653@gmail.com>
2023-08-03 00:01:21 +0530

    done travel, home, sports and news page iframe and styling. Added marquee tag for travel page and wrote js for this.

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-document.getElementById("livecnn");
-function
+
+document.getElementById("travelHeadline2").onmouseover = function(){
+document.getElementById("travelHeadline2").stop();
+}
+document.getElementById("travelHeadline2").onmouseleave = function(){
+document.getElementById("travelHeadline2").start();
+}
